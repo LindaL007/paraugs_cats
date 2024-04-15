5889dd3431bc7bbab2d560598b3d6a792c606404
LindaL007 <117632574+LindaL007@users.noreply.github.com>
2024-04-15 15:39:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert(1);
+//console.log(1)
+function sutitZinu()
+{
+    console.log('sutitZinu()darbojas');
+    let zina = dokoment.querySelector('zina');
+    let zinas = dokoment.querySelector('cataZina');
+    zinas.innerHTML = zinas.innerHTML+ '<br>' +zina.value;
+}
