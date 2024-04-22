0849e067e139c763afb2daab9ab5ebc1398b5467
LindaL007 <117632574+LindaL007@users.noreply.github.com>
2024-04-22 15:30:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 //console.log(1)
+let zina = dokoment.querySelector('.zina');
+let zinas = dokoment.querySelector('.cataZina');
+
 function sutitZinu()
 {
     console.log('sutitZinu()darbojas');
     zinas.innerHTML = zinas.innerHTML+ '<br>' +zina.value;
+}
+function ieladeChataZinas()
+{
+    let datiNoServera = await fetch('chataZinas.txt');
+    let dati = await datiNoServera.text;
 }
